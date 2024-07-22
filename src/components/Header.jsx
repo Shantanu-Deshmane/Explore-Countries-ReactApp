c8657cb8c761b8bbc5628da65ffb98d63ada48d8
Shantanu-Deshmane <shantanudeshmane81@gmail.com>
2024-07-22 21:42:33 +0530

    Fuinal commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,13 @@
 import React, { useState } from 'react'
 
+
 function Header({mode}) {
 const [dark, setDark] = mode
 
-// if(dark) {
-//   document.body.classList.add('dark')
-// }else{
-//   document.body.classList.add('dark')
-
-// }
 
   return (
     <>
-            <div className={`flex h-20  shadow-md justify-between items-center w-full px-14 ${dark ? 'dark' : ''}`}>
+            <div className={`flex h-20  shadow-md justify-between items-center w-full px-14 ${dark ? 'header-dark' : ''}`}>
                 <p className='text-2xl font-bold'> Explore the World !</p>
                 <p className='cursor-pointer' onClick={() => {
                   setDark(!dark)
